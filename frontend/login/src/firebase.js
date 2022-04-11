e1a5ffea6e4c67f371fe200ce7a250291a6a889f
Him-dhiman <2019meb1262@iitrpr.ac.in>
2022-04-12 00:58:36 +0530

    new change

--- frontend/login/src/firebase.js
+++ frontend/login/src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDVOeijJgZb_dEdRdzimAocij-8DpioUGQ",
-  authDomain: "musicapp-v1.firebaseapp.com",
-  databaseURL: "https://musicapp-v1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "musicapp-v1",
-  storageBucket: "musicapp-v1.appspot.com",
-  messagingSenderId: "673565277127",
-  appId: "1:673565277127:web:cf15561a13259c471d37e2",
-  measurementId: "G-NP4N4NT6DM"
+  //put API keys here
 })
 
 export const auth = app.auth()
